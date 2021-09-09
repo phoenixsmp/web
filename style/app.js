69b43a84c06ca0151a0a53f3649abf32f504d83c
itsyashk1406 <90339044+phoenixsmp@users.noreply.github.com>
2021-09-09 11:56:10 +0530

    teamphoenix web
    teamphoenix web(og)

---
+++ style/app.js
@@ -0,0 +1,44 @@
+const navSlide= ()=>{
+    const burger=document.querySelector('.burger'); 
+    const nav = document.querySelector('.nav-links ');
+    const navLinks =document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=> {
+        // tooglenav
+        nav.classList.toggle('nav-open');
+
+    //   animate link 
+   navLinks.forEach((link,index) => {
+       if (link.style.animation) {
+           link.style.animation='';
+           } else{
+               link.style.animation= `navLinkFade 0.5s ease forwards  ${index/7+0.3}s`;    
+           }
+   });
+  burger.classList.toggle('toggle');            
+});
+
+}
+navSlide();     
+
+function toggle(){ 
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup=document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+const logo=document.querySelectorAll("#logo path")
+
+for (let i=0; i< logo.length; i++){
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
+
+
+const scrollY = document.body.style.top;
+document.body.style.position = '';
+document.body.style.top = '';
+window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+// happy
+
